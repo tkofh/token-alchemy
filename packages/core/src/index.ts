63ff9037405809584e8bc70d52f32067335c603f
Tim Morris <timthewebguy@gmail.com>
2023-10-01 10:14:32 -0400

    feat: convenience exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,8 @@
 export { createDictionary, deserializeDictionary } from './dictionary'
+export type {
+  TokenDictionary,
+  DesignTokens,
+  DesignToken,
+  ResolvedToken,
+  ResolvedTokenPathSegment,
+} from '@token-alchemy/types'
